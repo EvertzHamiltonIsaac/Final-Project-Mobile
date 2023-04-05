a6da44bcbccc57527a58556da4a9991f5356a5c3
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2023-04-04 20:59:54 -0400

    Feat: Logout Working!!

--- app/src/main/java/com/example/whatsapp_clone/MainActivity.java
+++ app/src/main/java/com/example/whatsapp_clone/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.whatsapp_clone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.whatsapp_clone.Adapters.FragmentsAdapter;
 import com.example.whatsapp_clone.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
         binding.viewpager.setAdapter(new FragmentsAdapter(getSupportFragmentManager()));
         binding.tablayaout.setupWithViewPager(binding.viewpager);
 
+        auth = FirebaseAuth.getInstance();
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+         inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this, "Setting Clicked", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
 }
