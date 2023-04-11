64ccd21185e843f913cb0bb873e51fa0b1950b4b
Yelysz <anyelystorres17@gmail.com>
2023-04-11 16:47:33 -0400

    GroupChatActivity

--- app/src/main/java/com/example/whatsapp_clone/GroupChatActivity.java
+++ app/src/main/java/com/example/whatsapp_clone/GroupChatActivity.java
@@ -3,13 +3,9 @@ package com.example.whatsapp_clone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.LinearLayout;
-
 import com.example.whatsapp_clone.Adapters.ChatAdapter;
 import com.example.whatsapp_clone.Models.MessageModel;
 import com.example.whatsapp_clone.databinding.ActivityGroupChatBinding;
@@ -19,7 +15,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.Date;
 
